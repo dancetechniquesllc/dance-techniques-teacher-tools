444f9abc09a03d34bb0c3efbd52fa678ddbd0059
Lexi King <lexiking@Lexi-King-2.local>
2026-07-24 15:06:35 -0500

    Remove redundant announcement sender label

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "my-dance-techniques-v145-vector-reaction-icons";
+const CACHE_NAME = "my-dance-techniques-v146-announcement-copy";
 const CORE_ASSETS = [
   "./",
   "./index.html",
