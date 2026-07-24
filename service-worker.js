47f844eca2e347a2a2b7c963d81a9e9e88327243
Lexi King <lexiking@Lexi-King-2.local>
2026-07-24 14:23:34 -0500

    Harden school calendar overrides across ISDs

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "my-dance-techniques-v133-schedule-calendar-fixes";
+const CACHE_NAME = "my-dance-techniques-v134-school-calendar-hardening";
 const CORE_ASSETS = [
   "./",
   "./index.html",
