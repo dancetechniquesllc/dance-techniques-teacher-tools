4a2d230154bc3bde45808d8df7c9a43f564f755f
Lexi King <lexiking@Lexi-King-2.local>
2026-07-24 14:52:49 -0500

    Refine Teacher Tools inbox cards

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "my-dance-techniques-v137-teacher-messages-redesign";
+const CACHE_NAME = "my-dance-techniques-v138-inbox-card-layout";
 const CORE_ASSETS = [
   "./",
   "./index.html",
