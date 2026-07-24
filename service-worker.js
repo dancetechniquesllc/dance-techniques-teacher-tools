b200b15795d88204d2c7618a5f3fa70039f77af7
Dance Techniques LLC <297468390+dancetechniquesllc@users.noreply.github.com>
2026-07-23 23:45:59 -0500

    Make dashboard notifications actionable

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "my-dance-techniques-v129-isolated-class-roster";
+const CACHE_NAME = "my-dance-techniques-v130-actionable-notifications";
 const CORE_ASSETS = [
   "./",
   "./index.html",
