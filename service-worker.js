c0ad9899da4f2be2bc76e49be1cc459ba20afb7f
Lexi King <lexiking@Lexi-King-2.local>
2026-07-24 14:48:24 -0500

    Redesign Teacher Tools messaging

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "my-dance-techniques-v134-school-calendar-hardening";
+const CACHE_NAME = "my-dance-techniques-v137-teacher-messages-redesign";
 const CORE_ASSETS = [
   "./",
   "./index.html",
