c13e94a18a4bc8600f2fe8f765da695c93bf9fa9
Lexi King <lexiking@Lexi-King-2.local>
2026-07-24 14:54:33 -0500

    Move announcement history action to card header

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "my-dance-techniques-v138-inbox-card-layout";
+const CACHE_NAME = "my-dance-techniques-v139-announcement-history-button";
 const CORE_ASSETS = [
   "./",
   "./index.html",
