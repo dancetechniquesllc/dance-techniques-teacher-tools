a405ee7e676214aecb8534e33990ab46f374a3ea
Lexi King <lexiking@Lexi-King-2.local>
2026-07-25 17:24:05 -0500

    Restore teacher bubbles to class cards

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "my-dance-techniques-v184-teacher-color-fallback";
+const CACHE_NAME = "my-dance-techniques-v185-class-teacher-bubbles";
 const CORE_ASSETS = [
   "./",
   "./index.html",
