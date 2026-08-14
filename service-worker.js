2dd527a0e7f855955fb476505eae2647a5e922e2
Lexi King <lexiking@Lexi-King-2.local>
2026-08-14 15:05:42 -0500

    Handle notification worker activation races

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "my-dance-techniques-v209-notification-registration-repair";
+const CACHE_NAME = "my-dance-techniques-v210-notification-activation-race";
 const CORE_ASSETS = [
   "./",
   "./index.html",
