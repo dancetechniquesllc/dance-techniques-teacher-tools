8d23fbd1bcfba6187a5361c13fe2fea62bd8f7cb
dancetechniquesllc <dancetechniquesllc@gmail.com>
2026-09-15 08:09:04 -0500

    Restore teacher-owned DT Tunes playlists on top of refund/sibling publishes

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "my-dance-techniques-v235-working-hours-review";
+const CACHE_NAME = "my-dance-techniques-v238-teacher-playlists-restored";
 const CORE_ASSETS = [
   "./teacher-work-clock.js",
   "./assets/event-check-in-out.png",
