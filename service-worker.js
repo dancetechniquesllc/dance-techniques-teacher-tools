f814cffb3abc7c89540009b75ac6dbf3aedddb57
dancetechniquesllc <dancetechniquesllc@gmail.com>
2026-09-16 07:46:37 -0500

    Restore teacher-owned DT Tunes playlists (with owner-controls fix)

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "my-dance-techniques-v240-preorder-label-button";
+const CACHE_NAME = "my-dance-techniques-v241-teacher-playlists-restored-2";
 const CORE_ASSETS = [
   "./teacher-work-clock.js",
   "./assets/event-check-in-out.png",
